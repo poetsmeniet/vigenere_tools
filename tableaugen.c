225b1ea218f520d2498a0b4d8a6931760c195542
Thomas Wink <thomas@geenbs.nl>
2018-11-22 13:04:32 +0100

    Added ascii offset to tableau index values

--- tableaugen.c
+++ tableaugen.c
@@ -8,7 +8,8 @@ typedef const unsigned int cuint;
 typedef unsigned int uint;
 typedef const char cchar;
 
-/* Populates the vigenere tableau 
+/* Populates the vigenere tableau
+ * Indexes to access tableau are the ascii values of chars
  * Returns: 1 on success
  *          0 on failure
  * */
@@ -17,32 +18,33 @@ _Bool populateTableau(cuint size, char (*tab)[size], cchar *alphabet)
     if(size == 0)
         return 0;
 
-    int i;
-    int j;
+    uint i;
+    uint j;
+    uint offset = (uint)alphabet[0];
 
-    for (j = 0; j < size; j++){
-        for (i = 0; i < size; i++){
+    for (j = offset; j < (size + offset); j++){
+        for (i = offset; i < (size + offset); i++){
             char newChar = alphabet[( (i + j) % size )];
             tab[j][i] = newChar;
         }
-        i = 0;
+        i = offset;
     }
 
     return 1;
 }
 
-void printTableau(cuint size, char (*tab)[size])
+void printTableau(cuint size, char (*tab)[size], uint offset)
 {
     uint i;
     uint j;
 
-    for (j = 0; j < size; j++){
-        for(i = 0; i < size; i++){
+    for (j = offset; j < (size + offset); j++){
+        for(i = offset; i < (size + offset); i++){
             printf("%c ", tab[j][i]);
         }
 
         printf("\n");
-        i = 0;
+        i = offset;
     }
 }
 
@@ -57,12 +59,10 @@ int main(void)
     _Bool ret = populateTableau(size, tab, alphabet);
     if(ret){
         printf("Tableau populated, printing..\n");
-        printTableau(size, tab);
+        printTableau(size, tab, (uint)alphabet[0]);
     }else{
         return EXIT_FAILURE;
     }
-
-
 
     return 0;
 }
