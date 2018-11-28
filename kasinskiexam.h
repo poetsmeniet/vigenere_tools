1100cff3f91430e115e8291f62a4954322b87da0
Thomas Wink <thomas@geenbs.nl>
2018-11-28 14:34:34 +0100

    Initial commit

---
+++ kasinskiexam.h
@@ -0,0 +1,9 @@
+#ifndef kasinskyexam_H_
+#define kasinskyexam_H_
+
+/* Determines most likeley key sizes using Kasinski's examination 
+ * Returns: void
+ * Input: cipherText, keyLengths array
+ * */
+extern void returnKeyLengths(char *cipherText, uint *keyLengths);
+#endif
